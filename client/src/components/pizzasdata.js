9d099ecefbc802d3ba20c3cd63675a22a618f9eb
nayanatara07 <nayana2017tara@gmail.com>
2024-01-14 22:14:20 +0530

    pizzasdata.js added as sample for testing

---
+++ client/src/components/pizzasdata.js
@@ -0,0 +1,80 @@
+[{
+    name: "Pepper Barbeque Chicken",
+    varients: ["small","medeium","large"],
+    prices: [
+    {
+    "small": 200,
+    "medium": 350,
+    "large": 400
+    }
+],
+category: "nonveg",
+image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+description: "Pepper Barbeque Chicken I Cheese"
+},{
+    name: "Non Veg Supreme",
+    varients: ["small","medeium","large"],
+    prices: [
+        {
+        "small": 200,
+        "medium": 350,
+        "large": 400
+        }
+    ],
+category: "nonveg",
+image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+description: "Non Veg Supreme I Cheese"
+},{
+    name: "Golden Corn Pizza",
+    varients: ["small","medeium","large"],
+    prices: [
+        {
+        "small": 200,
+        "medium": 350,
+        "large": 400
+        }
+    ],
+category: "veg",
+image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+description: "Golden Corn Pizza I Cheese"
+},{
+    name: "Galapeno & Red Paprika Pizza",
+    varients: ["small","medeium","large"],
+    prices: [
+        {
+        "small": 200,
+        "medium": 350,
+        "large": 400
+        }
+    ],
+category: "veg",
+image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+description: "Galapeno & Red Paprika Pizza I Cheese"
+},{
+    name: "Margerita Chicken Pizza",
+    varients: ["small","medeium","large"],
+    prices: [
+        {
+        "small": 200,
+        "medium": 350,
+        "large": 400
+        }
+    ],
+category: "nonveg",
+image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+description: "Margerita Chicken I Cheese"
+},{
+    name: "Double Cheeze Margherita Pizza",
+    varients: ["small","medeium","large"],
+    prices: [
+        {
+        "small": 200,
+        "medium": 350,
+        "large": 400
+        }
+    ],
+category: "veg",
+image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+description: "Double Cheeze Margherita Pizza I Cheese"
+}
+]
