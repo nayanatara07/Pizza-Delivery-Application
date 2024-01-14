0c5ccae2bebb859591445d0fbbcf854838c17bb0
nayanatara07 <nayana2017tara@gmail.com>
2024-01-15 01:32:33 +0530

    flex container created with variants and prices along with styling

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -4,6 +4,19 @@ export default function Pizza({pizza}) {
   return (
     <div>
         <h1>{pizza.name}</h1>
+        <img src={pizza.image} className='img-fluid' style={{height:'200px',width:'200px'}}/>
+
+        <div className='flex-container'>
+
+            <div className='w-100'>
+                <p>Variants</p>
+            </div>
+
+            <div className='w-100'>
+                <p>Prices</p>
+            </div>
+
+        </div>
     </div>
   )
 }
