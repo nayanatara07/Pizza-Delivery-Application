5a4b1943abf0b43062365744978f871701eb8b22
nayanatara07 <nayana2017tara@gmail.com>
2024-01-18 22:48:53 +0530

    getting data from the database to our port through server.js get function

--- models/pizzaModel.js
+++ models/pizzaModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const pizzaSchema = mongoose.schema({
+const pizzaSchema = mongoose.Schema({
 
     name: {type: String, require},
     varients: [],
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
-
+const Pizza = require('./models/pizzaModel');
 const db = require("./db")
-
 const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Server working" + port);
+app.get("/", (req, res) => {
+    res.send("Server working on port " + port);
+});
+
+app.get("/getpizzas", async (req, res) => {
+    try {
+        const docs = await Pizza.find({});
+        console.log(docs);
+        res.send(docs);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 const port = process.env.PORT || 5000;
- app.listen(port, () => 'Server running on port port');
+app.listen(port, () => console.log('Server running on port', port));
