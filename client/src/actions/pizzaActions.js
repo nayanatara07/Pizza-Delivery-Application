2c7b8a8f1ade7626bdf6b14292a35592878aa256
nayanatara07 <nayana2017tara@gmail.com>
2024-01-20 19:37:32 +0530

    adding proxy server in package.json

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -1,4 +1,12 @@
+import axios from "axios";
 export const getAllPizzas=()=>dispatch=>{
 
     dispatch({type:'GET_PIZZAS_REQUEST'})
+
+    try {
+       const response = axios.get('/api/pizzas/getpizzas')
+       dispatch({type:'GET_PIZZAS_SUCCESS'}) 
+    } catch (error) {
+        dispatch({type:'GET_PIZZAS_FAILED'})   
+    }
 }
