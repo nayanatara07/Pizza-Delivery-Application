b7e081b13f7fc6fd33507847a6b80bd75194644b
nayanatara07 <nayana2017tara@gmail.com>
2024-02-11 18:19:40 +0530

    basic inputs for register screen

--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -3,11 +3,14 @@ import React from 'react'
 export default function Registerscreen() {
   return (
     <div>
-        <div className="row justify-content-center">
-          <div className = "col-md-5">
+        <div className="row justify-content-center mt-5">
+          <div className = "col-md-5 mt-5">
 
             <div>
               <input type="text" placeholder="name" className="form-control"/>
+              <input type="text" placeholder="email" className="form-control"/>
+              <input type="text" placeholder="password" className="form-control"/>
+              <input type="text" placeholder="confirm password" className="form-control"/>
             </div>
           </div>
         </div>
