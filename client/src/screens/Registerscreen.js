19fb8c86b3d2d1e2c3b8fb84bc95502d60d2ee96
nayanatara07 <nayana2017tara@gmail.com>
2024-02-11 18:13:52 +0530

    intial creation of register screen

--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Registerscreen() {
   return (
     <div>
-        <h1>Registerscreen</h1>
+        <div className="row justify-content-center">
+          <div className = "col-md-5">
+
+            <div>
+              <input type="text" placeholder="name" className="form-control"/>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
